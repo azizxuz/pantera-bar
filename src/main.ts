e8126317e08e704aff117591bd42023da85d1715
mrxx1234m <uzbsamuraypubgm@gmail.com>
2026-04-29 17:02:24 +0500

    update

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,18 @@ import { main as seed } from "../prisma/seed";
 
 async function bootstrap() {
   await seed();
+
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ["http://localhost:5173", "http://localhost:8080"],
+    origin: true, // barcha domenlar
     credentials: true,
   });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`🚀 Backend on :${process.env.PORT ?? 3000}`);
 }
+
 bootstrap();
